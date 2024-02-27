97ab4b141303d6f0bb8a1c2ec88ede8038a08657
svisvah <vishwa.s@easyoutdesk.com>
2024-02-27 18:29:56 +0530

    After completing header design

---
+++ app/code/Vendor/ShopByCategory/Block/Categorydropdown.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Vendor\ShopByCategory\Block;
+
+use Magento\Catalog\Model\CategoryFactory;
+use Magento\Framework\View\Element\Template;
+
+class Categorydropdown extends Template
+{
+    /**
+     * @var CategoryFactory
+     */
+    protected $categoryFactory;
+    /**
+     * @param Template\Context $context
+     * @param CategoryFactory $categoryFactory
+     * @param array $data
+     */
+    public function __construct(
+        Template\Context $context,
+        CategoryFactory $categoryFactory,
+        array $data = []
+    ) {
+        $this->categoryFactory = $categoryFactory;
+        parent::__construct($context, $data);
+    }
+    /**
+     * Retrieve the discount percentage for the current product
+     *
+     * @return array
+     */
+    public function getCategories()
+    {
+        $collection = $this->categoryFactory->create()->getCollection()
+            ->addAttributeToSelect('*')
+            ->addAttributeToFilter('is_active', 1); // Filter out disabled categories
+
+        return $collection;
+    }
+    /**
+     * Retrieve the discount percentage for the current product
+     *
+     * @return array
+     * @param int $categoryId
+     */
+    public function getSubcategories($categoryId)
+    {
+        $category = $this->categoryFactory->create()->load($categoryId);
+
+        // Check if the category has children
+        if ($category->hasChildren()) {
+            return $category->getChildrenCategories();
+        }
+
+        return [];
+    }
+}
---
+++ app/code/Vendor/ShopByCategory/Controller/Index/CategoryDropdown.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Vendor\ShopByCategory\Controller\Index;
+
+/**
+ * Class Index
+ * @package Haryasin\EventPackage\Controller\Index
+ */
+class CategoryDropdown extends \Magento\Framework\App\Action\Action
+{
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * Constructor
+     * @param \Magento\Framework\App\Action\Context $context
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     */
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+    ) {
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * Execute view action
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+        return $this->resultPageFactory->create();
+    }
+}
